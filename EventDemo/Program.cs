5e0feb7e2dc0f1a50b91c2cc41920f5b70ed0d22
hsh <sung5342@hanmail.net>
2019-07-03 12:19:49 +0900

    Add project files.

---
+++ EventDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using SortLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = { 3, 5, 1, 2 };
+
+            Sorter.Sort(ar, InAscending);
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.WriteLine(ar[i]);
+            }
+        }
+
+        private static bool InAscending(int i, int j)
+        {
+            return i > j;
+        }
+
+        private static bool InDescending(int i, int j)
+        {
+            return i < j;
+        }
+    }
+}
---
+++ SortLibrary/Sorter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortLibrary
+{
+    // public delegate bool Swapable(int i, int j);
+    public class Sorter
+    {
+        public static void Sort(int[] ar, Func<int, int, bool> swapable)
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j  < ar.Length; j ++)
+                {
+                    if(swapable(ar[i], ar[j]))
+                    {
+                        int t = ar[i];
+                        ar[i] = ar[j];
+                        ar[j] = t;
+                    }
+                }
+
+            }
+        }
+    }
+}
